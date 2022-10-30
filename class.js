0478e9379cb3aced81b1ca5e7c86e819a0724299
Enrique HM <enriquehmsb@gmail.com>
2022-10-30 02:16:20 -0600

    4taEntregaOk

--- class.js
+++ class.js
@@ -1,43 +1,73 @@
 let fs = require("fs");
+class Contenedor {
+  constructor() {}
+  save(producto) {
+    try {
+      let all = this.getAll();
+      const id = all.length + 1;
+      producto.id = id;
+      all.push(producto);
+      let products = JSON.stringify(all);
+      fs.writeFileSync("./productos.txt", products);
+    } catch {
+      console.log(err);
+    }
+  } /*  */
+  getById(number) {
+    try {
+      const datas = fs.readFileSync("./productos.txt", "utf-8");
+      let datasq = JSON.parse(datas);
+      let buscaPmostrar = datasq.findIndex((el) => el.id == number);
+      return buscaPmostrar > 0 ? datasq.find((el) => el.id == number) : console.log("producto no existe");
+    } catch {
+      console.log(err);
+    }
+  }
+  getAll(act) {
+    try {
+      switch (act) {
+        case undefined:
+          const datas = fs.readFileSync("./productos.txt", "utf-8");
+          let datasq = JSON.parse(datas);
+          return datasq;
+          break;
+        case "./perfiles.txt":
+          const datasP = fs.readFileSync(act, "utf-8");
+          let perfiles = JSON.parse(datasP);
+          return perfiles;
+          break;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  deleteById(aBorrar) {
+    try {
+      const datas = fs.readFileSync("./productos.txt", "utf-8");
+      let datasq1 = JSON.parse(datas);
+      let buscaPborrar = datasq1.findIndex((el) => el.id == aBorrar);
+      buscaPborrar > 0 ? datasq1.splice(buscaPborrar, 1) : console.log("producto no existe");
+      let documentAc = JSON.stringify(datasq1);
+      fs.writeFileSync("./productos.txt", `${documentAc}`);
+    } catch {
+      console.log(err);
+    }
+  }
+  deleteAll() {
+    try {
+      fs.writeFileSync("./productos.txt", JSON.stringify([]), console.log("Archivo vaciado correctamente"));
+    } catch {
+      console.log(err);
+    }
+  }
+  saveProduct(product) {
+    let all = this.getAll("./perfiles.txt");
+    const id = all.length + 1;
+    product.id = id;
+    all.push(product);
+    let products = JSON.stringify(all);
+    fs.writeFileSync("./perfiles.txt", products);
+  }
+}
 
-const getById = (number) => {
-  try {
-    const datas = fs.readFileSync("./productos.txt", "utf-8");
-    let datasq = JSON.parse(datas);
-    let buscaPmostrar = datasq.findIndex((el) => el.id == number);
-    return buscaPmostrar > 0 ? datasq.find((el) => el.id == number) : console.log("producto no existe");
-  } catch {
-    console.log(err);
-  }
-};
-const getAll = () => {
-  try {
-    const datas = fs.readFileSync("./productos.txt", "utf-8");
-    let datasq = JSON.parse(datas);
-    return datasq;
-  } catch (err) {
-    console.log(err);
-  }
-};
-const deleteById = (aBorrar) => {
-  try {
-    const datas = fs.readFileSync("./productos.txt", "utf-8");
-    let datasq1 = JSON.parse(datas);
-    let buscaPborrar = datasq1.findIndex((el) => el.id == aBorrar);
-    buscaPborrar > 0 ? datasq1.splice(buscaPborrar, 1) : console.log("producto no existe");
-    let documentAc = JSON.stringify(datasq1);
-    fs.writeFileSync("./productos.txt", `${documentAc}`);
-  } catch {
-    console.log(err);
-  }
-};
-const deleteAll = () => {
-  try {
-    fs.writeFileSync("./productos.txt", JSON.stringify([]), console.log("Archivo vaciado correctamente"));
-  } catch {
-    console.log(err);
-  }
-};
-
-let getAllOk = getAll();
-module.exports = getAllOk;
+module.exports = Contenedor;
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { Router } = express;
 const app = express();
 const routerDeProductos = Router();
-const crearPerfil = Router();
+const perfil = Router();
 const port = process.env.PORT || 8081;
 const multer = require("multer");
-const getAllOk = require("./class.js");
+const Contenedor = require("./class.js");
+const containerProducts = new Contenedor();
 
 let fs = require("fs");
 const { get } = require("https");
@@ -17,60 +18,65 @@ app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
 
+routerDeProductos.use((req, res, next) => {
+  console.log("Time:", Date());
+  next();
+});
 app.use("/api/productos", routerDeProductos);
-app.use("/crearPerfil", crearPerfil);
+app.use("/perfil", perfil);
 //para mostrar imagenes!!!!
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.send("HOLA puedes buscar endPoints: productos o productoRandom!");
+  res.send("HOLA puedes buscar endPoints: /api/productos,/api/productos/id, y /perfil/nuevo o /perfil/existentes ");
 });
+
 routerDeProductos.get("/", (req, res) => {
-  res.json(getAllOk);
+  res.json(containerProducts.getAll());
 });
 routerDeProductos.get("/:id", (req, res) => {
   const { id } = req.params;
-  let us = getAllOk.find((el) => el.id == id);
+  let us = containerProducts.getAll().find((el) => el.id == id);
   us != undefined ? res.json({ success: true, user: us }) : res.json({ err: true, msg: "no encontrado" });
 });
 routerDeProductos.post("/", (req, res) => {
   const { body } = req;
-  let all = getAllOk;
-  const id = all.length + 1;
-  body.id = id;
-  all.push(body);
-  let products = JSON.stringify(all);
-  fs.writeFileSync("./productos.txt", products);
-  res.send({ body });
+  containerProducts.save(body);
+  res.send({ id_Asignado: body.id });
 });
 routerDeProductos.put("/:id", (req, res) => {
   const { id } = req.params;
   const { body } = req;
-  let all = getAllOk;
-  let encontrado = all.findIndex((el) => el.id == id);
-  encontrado >= 0 ? (all[encontrado] = body) : res.json({ err: true, msg: "no encontrado" });
+  let all = containerProducts.getAll();
+  let product = all.findIndex((el) => el.id == id);
+  product >= 0 ? (all[product] = body) : res.json({ err: true, msg: "no encontrado" });
   let products = JSON.stringify(all);
   fs.writeFileSync("./productos.txt", products);
   res.json(body);
 });
 routerDeProductos.delete("/:id", (req, res) => {
   const { id } = req.params;
-  let arr = getAllOk;
+  let arr = containerProducts.getAll();
   let encontrado = arr.findIndex((el) => el.id == id);
-  arr.splice(encontrado, 1);
+  encontrado <= -1 ? res.json({ err: true, msg: "no encontrado" }) : arr.splice(encontrado, 1);
   let products = JSON.stringify(arr);
   fs.writeFileSync("./productos.txt", products);
-  res.send("ok");
+  res.send({ producto_con_id: id, msg: "eliminado" });
 });
+
 //FORMULARIO DEL INDEX RUTAS PARA CREAR PERFIL
-crearPerfil.get("/", (req, res) => {
+perfil.get("/nuevo", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+//ver perfiles ya creados
+perfil.get("/existentes", (req, res) => {
+  res.json(containerProducts.getAll("./perfiles.txt"));
 });
 
 //configuracion para subir foto
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads");
+    cb(null, __dirname + "/uploads/files");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -81,9 +87,7 @@ const upload = multer({ storage: storage });
 app.post("/uploadfile", upload.single("myFile"), (req, res) => {
   const file = req.file;
   const body = req.body;
-  if (!file) {
-    res.send({ error: true });
-  } else {
-    res.send(body + file.path);
-  }
+  body.imagen = file.filename;
+  containerProducts.saveProduct(body);
+  res.json({ message: "usuario guardado", usuario: body });
 });
