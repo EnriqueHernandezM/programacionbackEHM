415921e50454bcbe0cb2f7ee18772b774c6bc8af
Enrique Hernandez Montiel <106164484+EnriqueHernandezM@users.noreply.github.com>
2022-12-27 00:37:42 -0600

    Update mocks.js

--- src/mocksa/mocks.js
+++ src/mocksa/mocks.js
@@ -8,7 +8,7 @@ let str = "PRODUCTO;PRECIO;IMAGEN;DESCRIPTION;STOCKITEMS;DATA\n";
 for (let i = 0; i < 10; i++) {
   str += commerce.product() + ";" + finance.amount() + ";" + image.cats() + ";" + hacker.phrase() + ";" + date.past() + ";" + date.past() + "\n";
 }
-
+console.log("rrrr);
 writeFile("test.csv", str, (err) => {
   if (err) console.log(err);
   console.log("archivo guardado");
